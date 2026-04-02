13113403e20cd8b415aacc82dd2067c1e0dcff98
Hafsat Mukhtar <hafsatmukhtargaya@gmail.com>
2026-04-02 11:26:56 +0100

    Initialize project with HTML and JavaScript setup, test console connection, and implement getComputerChoice function using Math.random for random rock, paper, scissors selection

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+function getComputerChoice() {
+    let random = Math.random();
+    if(random < 0.33){
+        return "rock"
+    } else if (random < 0.66) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+console.log(getComputerChoice());
